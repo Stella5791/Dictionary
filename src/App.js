a49b169dc4927d6b0917e8ca29e7d42feef30216
Stella5791 <142344371+Stella5791@users.noreply.github.com>
2024-10-07 18:27:28 -0300

    added search engine

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
 import "./App.css";
+import "./Dictionary";
+import Dictionary from "./Dictionary";
 
 export default function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <div className="container">
+        <div className="main">
+          <Dictionary />
+        </div>
+        <div className="footer">Coded by Stella Bullo</div>
+      </div>
+    </div>
+  );
 }
